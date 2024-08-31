973d022da7b769b1c1b3adf0fb13117b04495382
susumu tomita <oyster880@gmail.com>
2024-08-31 11:00:13 +0900

    update logger

--- frontend/src/lib/logger.ts
+++ frontend/src/lib/logger.ts
@@ -1,23 +1,21 @@
 import winston from "winston";
 
+const safeStringify = (obj: any) => {
+  try {
+    return JSON.stringify(obj, null, 2);
+  } catch (error) {
+    return "Error in metadata serialization";
+  }
+};
+
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
     winston.format.colorize(),
     winston.format.splat(),
     winston.format.timestamp(),
-    const safeStringify = (obj) => {
-      try {
-        return JSON.stringify(obj, null, 2);
-      } catch (error) {
-        return 'Error in metadata serialization';
-      }
-    };
-
     winston.format.printf(({ timestamp, level, message, ...meta }) => {
-      const metaString = Object.keys(meta).length
-        ? safeStringify(meta)
-        : "";
+      const metaString = Object.keys(meta).length ? safeStringify(meta) : "";
       return `${timestamp} [${level}]: ${message} ${metaString}`;
     }),
   ),
