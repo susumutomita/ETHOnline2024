f33be87071a39a552c6eff930824ec17a64089df
susumu tomita <oyster880@gmail.com>
2024-09-01 11:58:51 +0900

    fix question not modify

--- frontend/src/app/(feedback)/feedback/preview/page.tsx
+++ frontend/src/app/(feedback)/feedback/preview/page.tsx
@@ -1,47 +1,28 @@
 "use client";
-
 import React from "react";
-import FeedbackFormFields from "@/components/FeedbackForm/FeedbackFormFields";
 import TokenDetailsForm from "@/components/FeedbackForm/TokenDetailsForm";
 import QuestionList from "@/components/FeedbackForm/QuestionList";
-import { useFeedbackContext } from "@/components/FeedbackForm/FeedbackContext"; // ここで正しくインポート
+import { useFeedbackContext } from "@/components/FeedbackForm/FeedbackContext";
 
 export default function PreviewPage() {
-  const {
-    productName,
-    category,
-    feedbackType,
-    tokenName,
-    price,
-    questions,
-    ratings,
-  } = useFeedbackContext(); // useFeedbackContextを利用してコンテキストを取得
+  const { tokenName, price, questions, ratings } = useFeedbackContext();
 
   return (
-    <div className="text-center">
+    <div className="text-center z-10">
       <h2 className="text-2xl font-bold mb-6">Preview Your Feedback Form</h2>
-      <FeedbackFormFields
-        productName={productName}
-        setProductName={() => {}}
-        category={category}
-        setCategory={() => {}}
-        feedbackType={feedbackType}
-        setFeedbackType={() => {}}
-        generateQuestions={() => {}}
-        isLoading={false}
-        readonly={true}
-      />
-      <TokenDetailsForm
-        tokenName={tokenName}
-        setTokenName={() => {}}
-        price={price}
-        setPrice={() => {}}
-        readonly={true}
-      />
+      {/* 質問のリストのみを表示 */}
       <QuestionList
         questions={questions}
         ratings={ratings}
-        setRatings={() => {}}
+        setRatings={() => {}} // プレビュー用なので、空の関数を渡す
+        readonly={true}
+      />
+      {/* トークンの詳細を表示 */}
+      <TokenDetailsForm
+        tokenName={tokenName}
+        setTokenName={() => {}} // プレビュー用なので、空の関数を渡す
+        price={price}
+        setPrice={() => {}} // プレビュー用なので、空の関数を渡す
         readonly={true}
       />
       <button className="bg-green-500 text-white p-2 rounded w-full mt-6">
